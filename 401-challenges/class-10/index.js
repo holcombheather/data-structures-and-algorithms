de3b8ee1e5f0f3e18772fd3803480b4babcc7af9
Heather Holcomb <holcombheather@gmail.com>
2023-05-27 15:42:10 -0700

    updated cc 10

---
+++ 401-challenges/class-10/index.js
@@ -0,0 +1,83 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    newNode.next = this.top;
+    this.top = newNode;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error('Stack is empty');
+    }
+    let poppedValue = this.top.value;
+    this.top = this.top.next;
+    return poppedValue;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      throw new Error('Stack is empty');
+    }
+    return this.top.value;
+  }
+
+  isEmpty() {
+    return this.top === null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+  }
+
+  enqueue(value) {
+    let newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.front = newNode;
+      this.rear = newNode;
+    } else {
+      this.rear.next = newNode;
+      this.rear = newNode;
+    }
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      throw new Error('Queue is empty');
+    }
+    let dequeuedValue = this.front.value;
+    this.front = this.front.next;
+    if (this.front === null) {
+      this.rear = null;
+    }
+    return dequeuedValue;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      throw new Error('Queue is empty');
+    }
+    return this.front.value;
+  }
+
+  isEmpty() {
+    return this.front === null;
+  }
+}
+
+module.exports = { Node, Stack, Queue };
